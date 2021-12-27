840d73cd4f8294ae31e3e5b5839e0eb64b845b70
Oksana Zelenevskaya <73756266+OksanaZel@users.noreply.github.com>
2021-12-27 16:47:09 +0200

    db connect

--- db/db.js
+++ db/db.js
@@ -1,13 +1,22 @@
 const Pool = require("pg").Pool;
+require("dotenv").config();
 
-const { POSTGRES_PORT, POSTGRES_USERNAME, POSTGRES_PASSWORD, POSTGRES_DATABASE, POSTGRES_HOST } = process.env;
+const { POSTGRES_PORT, POSTGRES_USERNAME, POSTGRES_PASSWORD, POSTGRES_DATABASE, POSTGRES_HOST, DATABASE_URL } = process.env;
 
+// const pool = new Pool({
+//   user: POSTGRES_USERNAME,
+//   password: POSTGRES_PASSWORD,
+//   host: POSTGRES_HOST,
+//   port: POSTGRES_PORT,
+//   database: POSTGRES_DATABASE,
+// });
+
+const connectionString = `postgresql://${process.env.POSTGRES_USERNAME}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DATABASE}`;
 const pool = new Pool({
-  user: POSTGRES_USERNAME,
-  password: POSTGRES_PASSWORD,
-  host: POSTGRES_HOST,
-  port: POSTGRES_PORT,
-  database: POSTGRES_DATABASE,
+    connectionString: DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 module.exports = pool;
