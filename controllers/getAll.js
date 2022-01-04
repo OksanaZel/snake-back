e01532bfb844e6a2fbbc045cacc08f7fa9f61a09
Oksana Zelenevskaya <73756266+OksanaZel@users.noreply.github.com>
2022-01-04 10:43:32 +0200

    add sorted

--- controllers/getAll.js
+++ controllers/getAll.js
@@ -1,9 +1,10 @@
+const { Op } = require('sequelize');
 const { NotFound } = require('http-errors');
 const { User } = require('../models/models');
 const { sendResponse } = require('../utils');
 
 const getAll = async (req, res) => {
-  const users = await User.findAll();
+  const users = await User.findAll({ where: { score: { [Op.gte]: 100 } } });
 
   if (!users) {
     throw new NotFound('Users not found');
